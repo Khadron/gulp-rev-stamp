5eb834cc2d072d4193f437e92a32c41fb5b67693
khadron <khadron@163.com>
2018-01-04 13:46:29 +0800

    remove the test file

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var stamp = require('./index');
-
-gulp.task("build", function() {
-    gulp.src('./test/index.html').pipe(stamp('$hehe$')).pipe(gulp.dest('./test/dist'));
-});
